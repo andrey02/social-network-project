a1ff55d64262aa7b17f336379cb6c3cea06dccb4
andrey02 <andreiteixeira@rocketmail.com>
2019-11-19 23:21:06 -0300

    testing some changes

---
+++ src/main/java/com/andrei/socialnetwork/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.andrei.socialnetwork.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class TestController {
+	
+	public void testMethod() {
+		System.out.println("Testing");
+	}
+
+}
